e80eb542f6d0e5ff4689e41de19f355ae1dac4ee
vivek <vivekchavan382@gmail.com>
2024-05-09 04:41:01 +0530

    minor changes

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import {AdminRoutes,VendorRoutes} from './routes'
+import { MONGO_URI } from './config';
 const SERVER_PORT: number = 3000;
 
 const app: Express = express();
@@ -13,6 +14,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", AdminRoutes)
 app.use("/vendor", VendorRoutes)
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(result=>{
+   console.log(result);
+}).catch(err => console.error(err));
+
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server running at: http://localhost:${SERVER_PORT}`);
